558d9bd69da84c7828b4f871ab3b654320c4e11d
syed hadi alvi <hadi.alvi@devsinc.com>
2024-01-05 13:24:21 -0500

    test-19

--- src/main.py
+++ src/main.py
@@ -17,6 +17,10 @@ def read_item(item_id: int, query_param: str = None):
 def create_item(item: dict):
     return {"item": item}
 
+@app.post("/items/test/test1")
+def create_item(item: dict):
+    return {"item": item}
+
 
 # def save_openapi_to_file():
 #     openapi_content = get_openapi(title="Lazaza", version="1.0.0",routes=app.routes)
@@ -24,7 +28,6 @@ def create_item(item: dict):
 #         json.dump(openapi_content, file, ensure_ascii=False, indent=2)
 
 def save_openapi_to_file():
-    # Check if openapi.json exists
     existing_content = None
     try:
         with open("openapi.json", "r", encoding="utf-8") as existing_file:
@@ -32,39 +35,35 @@ def save_openapi_to_file():
     except FileNotFoundError:
         pass  # File not found, no existing content
 
-    # Create a new openapi-new.json file
     new_file_path = "openapi-new.json"
     with open(new_file_path, "w", encoding="utf-8") as new_file:
         openapi_content = get_openapi(title="Lazaza", version="1.0.0", routes=app.routes)
         json.dump(openapi_content, new_file, ensure_ascii=False, indent=2)
 
-    # Check if both files have the same content
     if existing_content is not None and existing_content == openapi_content:
         print("No changes detected. Deleting the temporary file.")
-        # Clean up: Delete the temporary file
         import os
         os.remove(new_file_path)
         return False
 
-    # Update openapi.json with the content of openapi-new.json
     with open(new_file_path, "r", encoding="utf-8") as new_file:
         new_content = json.load(new_file)
         with open("openapi.json", "w", encoding="utf-8") as existing_file:
             json.dump(new_content, existing_file, ensure_ascii=False, indent=2)
 
-    # Clean up: Delete the temporary file
     import os
     os.remove(new_file_path)
     
     return True
 
-
 def get_openapi_status():
-    # Run the script and handle specific cases without using exceptions
     changes_detected = save_openapi_to_file()
     print(f"Changes detected: {changes_detected}")
     exit_code = 0 if not changes_detected else 1
     exit(exit_code)
+
+
+get_openapi_status()
 
 
 
