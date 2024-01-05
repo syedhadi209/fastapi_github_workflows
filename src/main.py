3d5f11d20de525a32cf6b7db60385a6347878bdc
syed hadi alvi <hadi.alvi@devsinc.com>
2024-01-05 08:38:04 -0500

    test-13

--- src/main.py
+++ src/main.py
@@ -10,6 +10,6 @@ def read_root():
 
 
 def save_openapi_to_file():
-    openapi_content = get_openapi(title="Lazaza", version="1.0.0",routes=app.routes)
+    openapi_content = get_openapi(title="Lazaza test", version="1.0.0",routes=app.routes)
     with open("openapi.json", "w", encoding="utf-8") as file:
         json.dump(openapi_content, file, ensure_ascii=False, indent=2)
