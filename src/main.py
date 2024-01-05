4dc0f6fb285dced4fbce9ef4e7c9dcab24bc21a3
syed hadi alvi <hadi.alvi@devsinc.com>
2024-01-05 07:57:21 -0500

    test-9

--- src/main.py
+++ src/main.py
@@ -12,9 +12,6 @@ def read_root():
 def read_item(item_id: int, query_param: str = None):
     return {"item_id": item_id, "query_param": query_param}
 
-@app.post("/items/")
-def create_item(item: dict):
-    return {"item": item}
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: dict):
